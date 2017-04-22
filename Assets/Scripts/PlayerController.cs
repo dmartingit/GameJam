74d96ffb273cc5baedf5b2c1fc292137565e7981
Daniel Martin <daniel.martin@hicknhack-software.com>
2017-04-22 17:23:52 +0200

    * Added simple movent.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam {
+    public class PlayerController : MonoBehaviour {
+
+        private float velocityX = 0.05f;
+        private float velocityY = 0.05f;
+        private bool flipDirection = false;
+
+        private Animator animator;
+
+        public bool GetRigth()
+        {
+            return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        }
+
+        public bool GetLeft()
+        {
+            return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        }
+
+        public bool GetUp()
+        {
+            return Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W);
+        }
+
+        private void Start()
+        {
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (GetRigth())
+            {
+                animator.SetInteger("Transition",1);
+                if(flipDirection)
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                    flipDirection = !flipDirection;
+                }
+                transform.Translate(velocityX, 0, 0);
+            } else if( GetLeft())
+            {
+                animator.SetInteger("Transition", 1);
+                if (!flipDirection)
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                    flipDirection = !flipDirection;
+                }
+                transform.Translate(-velocityX, 0, 0);
+            }
+            else
+            {
+                animator.SetInteger("Transition", 0);
+            }
+        }
+    }
+}
