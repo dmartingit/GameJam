f15f687244ebe69968403576baa09ff1ee033a29
Jörg Eckhold <joerg_eckhold@web.de>
2017-04-22 18:11:41 +0200

    "Jump"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameJam {
-    public class PlayerController : MonoBehaviour {
+    public class PlayerController : MonoBehaviour
+    {
 
         private float velocityX = 0.05f;
         private float velocityY = 0.05f;
+        private bool m_grounded = true;
+        private Collider2D m_collider;
         private bool flipDirection = false;
 
+        public LayerMask WhatIsGround;
+        private Transform GroundCheck;
         private Animator animator;
+
+        private const float GROUND_CHECK_RADIUS = 0.01f;
+
 
         public bool GetRigth()
         {
@@ -35,14 +43,15 @@ namespace GameJam {
         {
             if (GetRigth())
             {
-                animator.SetInteger("Transition",1);
-                if(flipDirection)
+                animator.SetInteger("Transition", 1);
+                if (flipDirection)
                 {
                     GetComponent<SpriteRenderer>().flipX = false;
                     flipDirection = !flipDirection;
                 }
                 transform.Translate(velocityX, 0, 0);
-            } else if( GetLeft())
+            }
+            else if (GetLeft())
             {
                 animator.SetInteger("Transition", 1);
                 if (!flipDirection)
@@ -52,10 +61,30 @@ namespace GameJam {
                 }
                 transform.Translate(-velocityX, 0, 0);
             }
+            else if (GetUp())
+            {
+                animator.SetInteger("Transition", 1);
+                transform.Translate(0, velocityX, 0);
+
+            }
             else
             {
                 animator.SetInteger("Transition", 0);
             }
+        }
+        private bool isGroundet()
+        {
+            m_collider = Physics2D.OverlapCircle(GroundCheck.position, GROUND_CHECK_RADIUS, WhatIsGround);
+            if (m_collider != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
         }
     }
 }
