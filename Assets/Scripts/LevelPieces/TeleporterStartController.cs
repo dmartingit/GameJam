9c8a7b9c78f9931589533a634dfc5ef0a9e97d05
Daniel Martin <daniel.martin@hicknhack-software.com>
2017-04-22 16:50:35 +0200

    * Added simple teleporter.

---
+++ Assets/Scripts/LevelPieces/TeleporterStartController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunAndJump
+{
+
+    public class TeleporterStartController : MonoBehaviour
+    {
+
+        public delegate void StartInteractionDelegate();
+        public static event StartInteractionDelegate StartInteractionEvent;
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                PlayerController player = col.gameObject.GetComponent<PlayerController>();
+                // Set big jump true
+                player.setDoubljump(true);
+
+                TeleporterExitController tpexit = FindObjectOfType<TeleporterExitController>();
+                player.transform.position = tpexit.transform.position;
+
+                //Destroy(gameObject);
+            }
+        }
+    }
+}
