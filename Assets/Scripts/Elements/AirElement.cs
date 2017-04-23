d2fa80b5c28241fdc703a0a05048563f2d3f8d49
Daniel Martin <daniel.martin@hicknhack-software.com>
2017-04-23 14:29:17 +0200

    * added missing elements.

---
+++ Assets/Scripts/Elements/AirElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+    public class AirElement : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                var player = col.gameObject.GetComponent<PlayerController>();
+                player.m_state = PlayerController.state.air;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
